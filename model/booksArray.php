01e57f992464e83e9f4d56714510f768d3da20b2
Ekaterina Lyamina <ekaterina_2001@inbox.ru>
2021-10-21 11:25:30 +0300

    MVC2

---
+++ model/booksArray.php
@@ -0,0 +1,32 @@
+<?php
+$books = array(
+    array("bookname" => "HTML and CSS",
+          "author" => "Jon Ducett",
+          "price" => "17.19$",
+          "description" => "Every day, more and more people want to learn some HTML and CSS. Joining the profess."
+          "image" => "HTMLandCSS_Duckett.jpg"),
+
+    array("bookname" => "HTML5",
+          "author" => "Matthew MacDonald",
+          "price" => "27.65$",
+          "description" => "HTML5 is more than a markup language-it`s a collection of several independent web."
+          "image" => "HTML5TheMissing_NacDonald.jpg"), 
+          
+    array("bookname" => "Head First HTML5 Programmering",
+          "author" => "Eric Freeman, Elisabeth Robson",
+          "price" => "35.35$",
+          "description" => "HTML has been on a wild ride. Sure, HTML started as a markup language, but more."
+          "image" => "HeadFirstHTML5.jpg"),
+    
+    array("bookname" => "Head First JavaScript Programmering",
+          "author" => "Eric Freeman, Elisabeth Robson",
+          "price" => "37.60$",
+          "description" => "This brain-friendly guide teaches you everything from JavaScript language fundamenta."
+          "image" => "HeadFirstJavaScript.jpg"),
+
+    array("bookname" => "jQuery in Action",
+          "author" => "Bear Bibeault, Yehuda Katz",
+          "price" => "32.12$",
+          "description" => "A really good web development framework anticipates your needs. jQuery does more-it."
+          "image" => "JQueryInAction.jpg")
+);
